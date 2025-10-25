175566cbd8524237d5a6b8e87ba384df6d6280cb
Sandra <sandra@MacBook-Air-Sandra.local>
2025-10-26 00:16:09 +0200

    Remove all files from main branch

--- src/app/components/AlgorithmCard.tsx
+++
@@ -1,8 +0,0 @@
-const AlgorithmCard = () => {
-  return (
-    <div className="rounded-2xl flex items-center justify-center bg-[#323232] p-4 aspect-[1/1] w-40 h-40">
-      A
-    </div>
-  );
-};
-export default AlgorithmCard;
